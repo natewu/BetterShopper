71b39e7b4c639241cfbdf23f50b1fd0652c6473e
natewu <36091727+natewu@users.noreply.github.com>
2021-01-24 11:40:44 -0700

    Working image uploader

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,40 +2,50 @@ import React, { Component } from 'react';
 import "./Input.css";
 import Button from '@material-ui/core/Button';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import ImageClassification from "../ml/ImageClassification";
+
+
 
 export class Input extends Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-            file:null
-        }
+    state = {
+        image:[]
     }
 
-    onSubmit = (e) => {
+    async loadFile(){
+        console.log("file loading");
+        var input = document.getElementById("input");
+        console.log(input.files[0]);
+        this.setState({image: []});
+        // this.setState({image:  });
+        var reader = new FileReader();
+        console.log("Loading image");
+        reader.onloadend = () =>{
+            this.setState({image: this.state.image.concat(reader.result)})
+        }
+        console.log(this.state.image);
+    }
+
+    setImages = (e) => {
         e.preventDefault();
-        this.fileUpload(this.state.file).then((response)=>{
-            console.log(response.data);
-        })
-    }
+        this.setState({ image: [] });
+        const imageFiles = e.target.files;
+        const filesLength = imageFiles.length;
 
-    onChange = (e) => {
-        this.state({file:e.target.files[0]});
-    }
+        for(var i = 0; i < filesLength; i++) {
+            let reader = new FileReader();
+            let file = imageFiles[i];
 
-    fileUpload(file){
-        const url = "";
-        const formData = new FormData();
-        formData.append('file',file);
-        const config = {
-            headers: {
-                "content-type" : "multipart/form-data"
+            reader.onloadend = () => {
+                this.setState({image: this.state.image.concat(reader.result)});
             }
+
+            reader.readAsDataURL(file);
         }
-        // return post(url, config);
     }
+        
 
     render() {
+        let { image } = this.state;
         return (
             <div>
                 <p>Please upload product images here.</p>
@@ -50,12 +60,14 @@ export class Input extends Component {
                 <form className="uploadImage">
                     <CloudUploadIcon className="icon"/>
                     <div className="info">
-                        <input type="file" name="file" onChange={this.onChange}/>
+                        <input id="input" type="file" name="file" onChange={this.setImages}/>
                         <p>Drag images here</p>
                         <p style={{fontSize:"1.4vmin", color:"rgb(130 130 130)"}}>Max File size {} • JPG/JPEG, PNG</p>
                     </div>
                     <Button id="upload" variant="outlined" color="primary" style={{margin:"0 15px 0 auto"}}>Browse</Button>
                 </form>
+                {image.map(image => <img id="test" alt="test" src={image} style={{maxWidth:"100%", height:"auto"}}/>)}
+                <ImageClassification image={this.state.image}/>
             </div>
         )
     }
--- src/ml/ImageClassification.js
+++ src/ml/ImageClassification.js
@@ -6,27 +6,26 @@ import Button from '@material-ui/core/Button';
 export class ImageClassification extends Component {
     async upload() {
         console.log("upload");
-        const img = document.getElementById("img")
+        const img = document.getElementById("test");
+        console.log(img);
         const model = await mobilenet.load();
         const predictions = await model.classify(img);
         console.log('Predictions: ');
         console.log(predictions);
     }
 
-
     async init(){
         let model = undefined;
-        model = await tf.loadLayersModel("https://raw.githubusercontent.com/natewu/BetterShopper/master/src/ml/clothingModel/model.json");
+        model = await tf.loadLayersModel("./clothingModel/model.json");
         console.log("Model Loaded");
     }
- 
         
     render() {
-    
         return (
             <div>
-                <img id="img" alt="car" src="./shirt.png"/>
-                <Button onClick={async() => await this.upload()} variant="outlined" color="primary" style={{margin:"0 15px 0 auto"}}>Browse</Button>
+                {/* <img id="img" alt="car" src="./shirt.png"/> */}
+                {/* <Input data={this.props.image}/> */}
+                <Button onClick={async() => await this.upload()} variant="outlined" color="primary" style={{margin:"0 15px 0 auto"}}>Predict</Button>
                 {/* {this.init()} */}
             </div>
         )
